587242f658c75f5414de3a97079963d022ea9c55
Kim Seung Hyun <kaka@Kimui-Macmini.local>
2025-01-26 17:08:50 +0900

    calendar button 수정

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
@@ -54,11 +54,15 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn('h-4 w-4', className)} {...props} />
+        PreviousMonthButton: (props) => (
+          <Button {...props}>
+            <ChevronLeft className="h-4 w-4" />
+          </Button>
         ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn('h-4 w-4', className)} {...props} />
+        NextMonthButton: (props) => (
+          <Button {...props}>
+            <ChevronRight className="h-4 w-4" />
+          </Button>
         ),
       }}
       {...props}
