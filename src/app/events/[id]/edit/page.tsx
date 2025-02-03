be27a50c1e3aeccf74dbcf82ee035abfb5180f0b
Kim Seung Hyun <iamkimseunghyun@gmail.com>
2025-02-03 13:55:30 +0900

    params Promise 선언

--- src/app/events/[id]/edit/page.tsx
+++ src/app/events/[id]/edit/page.tsx
@@ -3,7 +3,7 @@ import { getEventById } from '@/app/events/actions';
 import { notFound } from 'next/navigation';
 import { EventFormWrapper } from '@/components/page/event/event-form-wrapper';
 
-const Page = async ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
   const [eventResult, venues, artists] = await Promise.all([
