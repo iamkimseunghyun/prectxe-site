03887f75ad4d586227de6048015696843c83ed18
Kim Seung Hyun <iamkimseunghyun@gmail.com>
2025-02-10 18:44:24 +0900

    로그인/가입 창 위치 조정

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
   const [state, action] = useActionState(signUp, null);
 
   return (
-    <div className="flex h-screen flex-col justify-center gap-10 px-6 py-8">
+    <div className="flex h-screen flex-col justify-start gap-10 px-6 py-8 pt-20">
       <div className="flex flex-col gap-2 text-center *:font-medium">
         <h1>안녕하세요!</h1>
         <h2>아래 정보를 입력하고 회원가입을 완료하세요!</h2>
