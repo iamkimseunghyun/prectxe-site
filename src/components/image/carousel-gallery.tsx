796af881a4341d56aa3c65cad311c637ae850d7b
Kim Seung Hyun <iamkimseunghyun@gmail.com>
2025-03-08 17:21:36 +0900

    project 상세페이지 갤러리 비율 16/10.5로 수정

--- src/components/image/carousel-gallery.tsx
+++ src/components/image/carousel-gallery.tsx
@@ -16,17 +16,31 @@ const CarouselGallery = ({ images }: CarouselProps) => {
   return (
     <div className="h-full w-full" style={{ minHeight: '100%' }}>
       <ShadcnCarousel className="h-full w-full">
+        {/*<CarouselContent>*/}
+        {/*  {images.map((image, index) => (*/}
+        {/*    <CarouselItem key={index}>*/}
+        {/*      <div className="relative aspect-square sm:aspect-video">*/}
+        {/*        <Image*/}
+        {/*          src={getImageUrl(image.imageUrl, 'public')}*/}
+        {/*          alt={image.alt || '장소 이미지'}*/}
+        {/*          width={1200}*/}
+        {/*          height={800}*/}
+        {/*          className="rounded-lg object-cover"*/}
+        {/*          priority={index === 0}*/}
+        {/*        />*/}
+        {/*      </div>*/}
+        {/*    </CarouselItem>*/}
+        {/*  ))}*/}
+        {/*</CarouselContent>*/}
         <CarouselContent>
           {images.map((image, index) => (
             <CarouselItem key={index}>
-              <div className="relative aspect-square sm:aspect-video">
-                <Image
+              <div className="relative aspect-square sm:aspect-[16/10.5]">
+                <img
                   src={getImageUrl(image.imageUrl, 'public')}
                   alt={image.alt || '장소 이미지'}
-                  width={1200}
-                  height={800}
-                  className="rounded-lg object-cover"
-                  priority={index === 0}
+                  className="absolute inset-0 h-full w-full rounded-lg object-cover"
+                  loading={index === 0 ? 'eager' : 'lazy'}
                 />
               </div>
             </CarouselItem>
